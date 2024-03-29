6913a2e3e79c2de9bb18a14b4e66a5d9ca93a49e
Fahmi Hasan <mochammadfahmihasan@gmail.com>
2024-03-29 23:16:27 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+axios.defaults.baseURL = process.env.REACT_APP_API_URL
+
+axios.interceptors.request.use(function (config) {
+  config.headers['X-Binarybox-Api-Key'] = process.env.REACT_APP_API_KEY;
+  return config;
+});
+
 root.render(
   <React.StrictMode>
     <App />
